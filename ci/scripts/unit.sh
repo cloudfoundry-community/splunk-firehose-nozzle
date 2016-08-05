a256a33fb2e2ab90122e9b7a572ff30bbeaa5e88
Matt Cholick <cholick@gmail.com>
2016-08-05 14:37:26 -0700

    Iterating on test script

--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -2,12 +2,6 @@
 
 set -ex
 
-echo "Installing golang test tools"
-echo ""
-go get github.com/onsi/ginkgo/ginkgo
-go get github.com/onsi/gomega
-
-
 echo "Jumping through hoops for golang"
 export GOPATH=`pwd`
 cp -r firehose-repo splunk-firehose-nozzle
@@ -15,6 +9,12 @@ mkdir -p src/github.com/cf-platform-eng
 mv splunk-firehose-nozzle src/github.com/cf-platform-eng
 
 cd src/github.com/cf-platform-eng/splunk-firehose-nozzle
+
+
+echo "Installing golang test tools"
+echo ""
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/gomega
 
 
 echo "Building"
