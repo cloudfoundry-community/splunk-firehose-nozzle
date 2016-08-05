238944628523f7d4757ed455a0d5e3b6d108ae17
Matt Cholick <cholick@gmail.com>
2016-08-05 14:14:26 -0700

    Iterating on test script

--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 echo "Installing golang test tools"
+echo ""
 go get github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega
 
 echo "Building"
+echo ""
+go get github.com/Masterminds/glide
+
 cd firehose-repo
+glide install
 go build main.go
 
 echo "Testing"
+echo ""
 ginkgo -r
