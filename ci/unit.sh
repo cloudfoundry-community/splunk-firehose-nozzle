c126c10e56c33f0f12345017ef030fef8d329d4f
Matt Cholick <cholick@gmail.com>
2017-01-24 10:22:37 -0800

    Iterating on CI script

--- ci/unit.sh
+++ ci/unit.sh
@@ -6,9 +6,8 @@ echo "Fiddle with the go path"
 export GOPATH=`pwd`
 export PATH=$GOPATH/bin:$PATH
 
-cp -r source-repo/src/splunk-firehose-nozzle splunk-firehose-nozzle
 mkdir -p src/github.com/cloudfoundry-community
-mv splunk-firehose-nozzle src/github.com/cloudfoundry-community
+cp -r source-repo src/github.com/cloudfoundry-community/splunk-firehose-nozzle
 
 cd src/github.com/cloudfoundry-community/splunk-firehose-nozzle
 
