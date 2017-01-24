71648e142c5969e59c283b0073d2a0bfdaa3a3be
Matt Cholick <cholick@gmail.com>
2017-01-24 10:11:29 -0800

    Unit testing ci script

---
+++ ci/unit.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash -ex
+
+echo "Fiddle with the go path"
+export GOPATH=`pwd`
+export PATH=$GOPATH/bin:$PATH
+
+cp -r source-repo/src/splunk-firehose-nozzle splunk-firehose-nozzle
+mkdir -p src/github.com/cloudfoundry-community
+mv splunk-firehose-nozzle src/github.com/cloudfoundry-community
+
+cd src/github.com/cloudfoundry-community/splunk-firehose-nozzle
+
+
+echo "Installing Go test tools"
+echo ""
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/gomega
+
+
+echo "Building"
+echo ""
+go build main.go
+
+
+echo "Testing"
+echo ""
+ginkgo -r
