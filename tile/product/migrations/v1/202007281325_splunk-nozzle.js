db9d093bb43db5454b8c913c13fbd20f0992f963
Rock Baek <rbaek@splunk.com>
2020-07-28 13:52:29 -0700

    relocate migration folder

--- tile/product/migrations/v1/202007281325_splunk-nozzle.js
+++ tile/product/migrations/v1/202007281325_splunk-nozzle.js
@@ -1,5 +1,4 @@
 exports.migrate = function(input) {
-    // Append text to a string
     if (input.properties['.forms.{advanced}.properties.{add_app_info}']['value']=='true') {
         input.properties['.forms.{advanced}.properties.{add_app_info}']['value'] = "AppName,OrgName,OrgGuid,SpaceName,SpaceGuid"
     } else {
