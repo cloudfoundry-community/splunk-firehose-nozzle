6130840a5541ee696dfd15a906af8e477e5a20e8
Shubham Jain <shujain@splunk.com>
2020-07-28 15:53:09 -0700

    update tile with migration step

--- tile/product/migrations/v1/202007281325_splunk-nozzle.js
+++
@@ -1,8 +0,0 @@
-exports.migrate = function(input) {
-    if (input.properties['.forms.{advanced}.properties.{add_app_info}']['value']=='true') {
-        input.properties['.forms.{advanced}.properties.{add_app_info}']['value'] = "AppName,OrgName,OrgGuid,SpaceName,SpaceGuid"
-    } else {
-        input.properties['.forms.{advanced}.properties.{add_app_info}']['value'] = ""
-    }
-    return input;
-};
