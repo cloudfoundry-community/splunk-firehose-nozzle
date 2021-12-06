39ecfdcc70a309c750c68de7748a08fed5ffb5eb
Kashyap Solaki <ksolanki@splunk.com>
2021-12-06 10:37:12 +0530

    added a test for debug mode

--- eventwriter/splunk_test.go
+++ eventwriter/splunk_test.go
@@ -209,6 +209,15 @@ var _ = Describe("Splunk", func() {
 			Expect(err).To(BeNil())
 			Expect(capturedRequest.URL.Path).To(Equal("/services/collector"))
 		})
+
+		It("Writes to stdout in debug without error", func() {
+			config.Debug = true
+			client := NewSplunk(config)
+			events := []map[string]interface{}{}
+			err, _ := client.Write(events)
+
+			Expect(err).To(BeNil())
+		})
 	})
 
 	It("returns error on bad splunk host", func() {
