9e4fd653a89a81b156547b0d7e42317fd8b869c8
sbylica-splunk <sbylica@splunk.com>
2023-10-20 13:36:23 +0200

    Refactoring sh files

--- tile/build.sh
+++ tile/build.sh
@@ -9,11 +9,11 @@ fi
 
 echo "building go binary"
 pushd ..
-curdir=`pwd`
+curdir=$(pwd)
 go get github.com/cloudfoundry-community/splunk-firehose-nozzle
-cd $GOPATH/src/github.com/cloudfoundry-community/splunk-firehose-nozzle && git checkout master && env GOOS=linux GOARCH=amd64 make build VERSION=1.0.0
-cp $GOPATH/src/github.com/cloudfoundry-community/splunk-firehose-nozzle/splunk-firehose-nozzle ${curdir}/../splunk-firehose-nozzle/
-cd ${curdir}
+cd "$GOPATH/src/github.com/cloudfoundry-community/splunk-firehose-nozzle && git checkout master && env GOOS=linux GOARCH=amd64 make build VERSION=1.0.0"
+cp "$GOPATH/src/github.com/cloudfoundry-community/splunk-firehose-nozzle/splunk-firehose-nozzle ${curdir}/../splunk-firehose-nozzle/"
+cd "${curdir}"
 popd
 
 echo "building tile"
